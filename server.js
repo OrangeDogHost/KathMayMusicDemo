4ad73bfac2e9bea5b0b9ebeebb483c70ab1fc6da
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2024-08-31 13:42:13 +1200

    Conditional rendering

--- server.js
+++ server.js
@@ -39,27 +39,3 @@ app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
 
-// app.post('/formsubmit', (req, res) => {
-//     const eventDetails = {
-//       eventName: req.body.eventName,
-//       eventVenue: req.body.eventVenue,
-//       eventTime: req.body.eventTime,
-//       eventDate: req.body.eventDate,
-//       eventPrice: req.body.eventPrice,
-//       eventDescription: req.body.eventDescription
-//     };
-//     console.log(eventDetails);
-//     res.redirect(`/?eventName=${eventDetails.eventName}&eventVenue=${eventDetails.eventVenue}&eventTime=${eventDetails.eventTime}&eventDate=${eventDetails.eventDate}&eventPrice=${eventDetails.eventPrice}&eventDescription=${eventDetails.eventDescription}`);
-//   });
-  
-//   app.get("/", (req, res) => {
-//     const eventDetails = {
-//       eventName: req.query.eventName,
-//       eventVenue: req.query.eventVenue,
-//       eventTime: req.query.eventTime,
-//       eventDate: req.query.eventDate,
-//       eventPrice: req.query.eventPrice,
-//       eventDescription: req.query.eventDescription
-//     };
-//     res.render("index.ejs", { eventDetails });
-//   });
