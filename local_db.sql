9507a4d15c796d605368ac3145f0bf9e7142d508
Zack Palmer <zack@orangedog.co.nz>
2024-09-03 13:54:18 +1200

    add local db sql schema

---
+++ local_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS events ( name TEXT PRIMARY KEY, venue TEXT, time TEXT, date TEXT, price TEXT, description TEXT);
+
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee","venueeeee","timeeeee","dateeeee","99","descriptionnnnn");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee2","venueeeee2","timeeeee2","dateeeee2","99","descriptionnnnn2");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee3","venueeeee3","timeeeee3","dateeeee3","99","descriptionnnnn3");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee4","venueeeee4","timeeeee4","dateeeee4","99","descriptionnnnn4");
