ec172ec3078d11f28bc317433f37a4a81debc877
Zack Palmer <zack@orangedog.co.nz>
2024-09-03 13:00:25 +1200

    get all instead

--- functions/events.js
+++ functions/events.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
 	console.log(context.env);
 	const db = context.env.KATHY_DB.prepare("SELECT * FROM events");
-	const events = await db.first();
+	const events = await db;
 
 	return new Response(JSON.stringify(events));
 }
