51aa58f50ee4662d0a9c19d09ad1547f2de9fd41
davidbarnett <david@orangedog.co.nz>
2024-09-04 13:36:51 +1200

    Various Updates
    Resized Images, changed form to formspree, updated script file for gig information.

--- dist/script.js
+++ dist/script.js
@@ -11,30 +11,34 @@ const songs = [
   "sara"
 ];
 
-let events = fetch("/events").then(r=>r.json()).then(d=>d.results)
+// let events = fetch("/events").then(r=>r.json()).then(d=>d.results);
 
-const price = new Intl.NumberFormat('en-NZ', { style: 'currency', currency: 'NZD' })
+let events = fetch("/events").then(r => r.json()).then(d => d.results);
 
-document.addEventListener("DOMContentLoaded",()=>{
-    events.then(events=>{
-        let parent = document.querySelector('.gig-info')
-        console.log(parent)
-        let eventsHTML = ""
-        events.forEach(event=>{
-            console.log(event)
+const price = new Intl.NumberFormat('en-NZ', { style: 'currency', currency: 'NZD' });
+
+document.addEventListener("DOMContentLoaded", () => {
+    events.then(events => {
+        let parent = document.querySelector('.gig-info');
+        let eventsHTML = "";
+        events.forEach((event, idx) => {
+            console.log(event);
             eventsHTML += `<div class="info info-1">
                             <span>${price.format(event.price)}</span>
-                            <img src="assets/01.jpg" alt="">
+                            <img src="assets/0${idx + 1}.webp" alt="">
                             <div class="details">
                                 <h2>Gig Info</h2>
                                 <small>${event.date} / ${event.time} / ${event.venue}</small>
                                 <p style="color: white; font-size: 0.75rem; line-height: 20px; padding: 20px">${event.description}</p>
                             </div>
                         </div>`
-        })
-        parent.innerHTML = eventsHTML
+        });
+        parent.innerHTML = eventsHTML;
     })
+    
 })
+
+
 
 const progressContainer = document.querySelector('.progress-container'); 
 const songTitle = document.querySelector('.song-titles h3');
--- local_db.sql
+++ local_db.sql
@@ -1,7 +1,7 @@
 DROP TABLE events;
 CREATE TABLE IF NOT EXISTS events ( name TEXT PRIMARY KEY, venue TEXT, time TEXT, date TEXT, price TEXT, description TEXT);
 
-INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee","venueeeee","timeeeee","dateeeee","12","descriptionnnnn");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("Gig number 1", "Westpac Stadium", "12:00pm", "17th March 1986", "23", "Get your tickets at ticket master!");
 INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee2","venueeeee2","timeeeee2","dateeeee2","32","descriptionnnnn2");
 INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee3","venueeeee3","timeeeee3","dateeeee3","122","descriptionnnnn3");
 INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee4","venueeeee4","timeeeee4","dateeeee4","22","descriptionnnnn4");
