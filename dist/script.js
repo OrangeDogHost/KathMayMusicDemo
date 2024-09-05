384cd3f999237843aa9b813ce19e7ef1200e91e1
davidbarnett <david@orangedog.co.nz>
2024-09-05 12:34:17 +1200

    Added new copy

--- dist/script.js
+++ dist/script.js
@@ -17,36 +17,27 @@ let events = fetch("/events").then(r => r.json()).then(d => d.results);
 
 const price = new Intl.NumberFormat('en-NZ', { style: 'currency', currency: 'NZD' });
 
-document.addEventListener("DOMContentLoaded", () => {
-    events.then(data => {
-       if(data && data.length > 0) {
-        console.log("The events do something here")
-       }else{
-        console.log("The events do not exists");
-       }
-    }) 
-})
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     events.then(events => {
-//         let parent = document.querySelector('.gig-info');
-//         let eventsHTML = "";
-//         events.forEach((event, idx) => {
-//             console.log(event);
-//             eventsHTML += `<div class="info info-1">
-//                             <span>${price.format(event.price)}</span>
-//                             <img src="assets/0${idx + 1}.webp" alt="">
-//                             <div class="details">
-//                                 <h2>Gig Info</h2>
-//                                 <small>${event.date} / ${event.time} / ${event.venue}</small>
-//                                 <p style="color: white; font-size: 0.75rem; line-height: 20px; padding: 20px">${event.description}</p>
-//                             </div>
-//                         </div>`
-//         });
-//         parent.innerHTML = eventsHTML;
-//     })
+document.addEventListener("DOMContentLoaded", () => {
+    events.then(events => {
+        let parent = document.querySelector('.gig-info');
+        let eventsHTML = "";
+        events.forEach((event, idx) => {
+            console.log(event);
+            eventsHTML += `<div class="info info-1">
+                            <span>${price.format(event.price)}</span>
+                            <img src="assets/0${idx + 1}.webp" alt="">
+                            <div class="details">
+                                <h2>Gig Info</h2>
+                                <small>${event.date} / ${event.time} / ${event.venue}</small>
+                                <p style="color: white; font-size: 0.75rem; line-height: 20px; padding: 20px">${event.description}</p>
+                            </div>
+                        </div>`
+        });
+        parent.innerHTML = eventsHTML;
+    })
     
-// })
+})
 
 
 
--- local_db.sql
+++ local_db.sql
@@ -1,9 +1,6 @@
 DROP TABLE events;
 CREATE TABLE IF NOT EXISTS events ( name TEXT PRIMARY KEY, venue TEXT, time TEXT, date TEXT, price TEXT, description TEXT);
-DROP TABLE asdasd;
-CREATE TABLE IF NOT EXISTS asdasd ( name TEXT PRIMARY KEY, venue TEXT, time TEXT, date TEXT, price TEXT, description TEXT);
-
-INSERT INTO asdasd (name,venue,time,date,price,description) VALUES("Gig number 1", "Westpac Stadium", "12:00pm", "17th March 1986", "23", "Get your tickets at ticket master!");
-INSERT INTO asdasd (name,venue,time,date,price,description) VALUES("nameeeee2","venueeeee2","timeeeee2","dateeeee2","32","descriptionnnnn2");
-INSERT INTO asdasd (name,venue,time,date,price,description) VALUES("nameeeee3","venueeeee3","timeeeee3","dateeeee3","122","descriptionnnnn3");
-INSERT INTO asdasd (name,venue,time,date,price,description) VALUES("nameeeee4","venueeeee4","timeeeee4","dateeeee4","22","descriptionnnnn4");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("Gig number 1", "Westpac Stadium", "12:00pm", "17th March 1986", "23", "Get your tickets at ticket master!");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee2","venueeeee2","timeeeee2","dateeeee2","32","descriptionnnnn2");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee3","venueeeee3","timeeeee3","dateeeee3","122","descriptionnnnn3");
+INSERT INTO events (name,venue,time,date,price,description) VALUES("nameeeee4","venueeeee4","timeeeee4","dateeeee4","22","descriptionnnnn4");
